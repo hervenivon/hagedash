a83ad48220ce6b34c2ac8353eb1201c588f8a255
Hervé Nivon <herve.nivon@gmail.com>
2018-08-02 23:44:07 +0200

    update styling

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-// replace the default bootstrap them with https://bootswatch.com/
+// replace the default bootstrap them with flatly from https://bootswatch.com/
 // import 'bootstrap/dist/css/bootstrap.min.css';
-import './vendors/bootswatch/bootstrap.min.css';
+import './vendors/bootswatch/bootstrap.css';
 import configureStore from './store';
 import './index.css';
 import App from './containers/App';
